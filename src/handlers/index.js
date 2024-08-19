e52ef0aa459fd3556fa20bb4c501c1c911a6d3a7
Ravivarma <ryuk.noir@gmail.com>
2024-08-19 22:23:06 +0530

    Update index.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,9 +1,19 @@
-// src/handlers/handler.js
+// src/handlers/index.js
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
+  try {
+    // Your function logic here
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello from handler.js!" }),
+      body: JSON.stringify({ message: 'Success' }),
     };
-  };
-  
+  } catch (error) {
+    console.error('Error:', error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error', error: error.message }),
+    };
+  }
+};
