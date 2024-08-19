bbc883e1402e763382f21ebaf79c604247dfec3a
Ravivarma <ryuk.noir@gmail.com>
2024-08-20 01:55:14 +0530

    Update db.js

--- .netlify/services/db.js
+++ .netlify/services/db.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://Ravivarma:RavivarmaMongo@movie-phobics.x3v8z.mongodb.net/?retryWrites=true&w=majority&appName=movie-phobics"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
