ff01eb8067252105f2ed80fe3067ad78d0a898c2
Ravivarma <ryuk.noir@gmail.com>
2024-08-19 23:14:04 +0530

    made initial mongo connection

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,9 +1,21 @@
 // src/handlers/handler.js
 
+const { getData } = require("../services/db");
+
 exports.handler = async (event) => {
+  try {
+    console.log('Event', JSON.stringify(event, null, 2));
+    const data = await getData();
+    console.log('Data retrieved:', data);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello from handler.js!" }),
+      body: JSON.stringify({ message: JSON.stringify(event, null, 2) }),
     };
-  };
-  
+  }
+  catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: JSON.stringify(e, null, 2) }),
+    };
+  }
+};
