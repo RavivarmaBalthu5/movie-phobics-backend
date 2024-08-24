0fbc94f2aeb56f7247d257209c22b350686dbf53
Ravivarma <ryuk.noir@gmail.com>
2024-08-25 01:33:57 +0530

    made changes to get api from mongodb

--- .netlify/helpers/allMovies.js
+++ .netlify/helpers/allMovies.js
@@ -4,7 +4,7 @@ const { prepareResponse } = require("../utils/utils");
 
 const getInitialMovies = async () => {
     try {
-        let searchResponse = await find(MOVIE_COLLECTION, {}, null, DEFAULT_LIMIT);
+        let searchResponse = await find(MOVIE_COLLECTION, {}, { "title": 1, "poster_path": 1, "id": 1, "release_date": 1 }, DEFAULT_LIMIT);
         if (searchResponse === 0) {
             return prepareResponse(400, { error: 'Movies not found' })
         }
