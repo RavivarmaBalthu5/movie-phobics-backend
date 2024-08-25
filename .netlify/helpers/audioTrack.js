2a4a6f2c104939fe9859da78cccabaeceb706964
Ravivarma <ryuk.noir@gmail.com>
2024-08-25 22:45:12 +0530

    added new api to fetch songs

---
+++ .netlify/helpers/audioTrack.js
@@ -0,0 +1,37 @@
+const { getSpotifyToken } = require("../services/apiService");
+const { upsertDocuments, find } = require("../services/db");
+const { TRACKS_COLLECTION, DEFAULT_LIMIT, SPOTIFY_SEARCH_URL, TRACK_SEARCH_PROJECTION } = require("../utils/constants");
+const { prepareResponse } = require("../utils/utils");
+const axios = require('axios');
+const _ = require('lodash')
+const querystring = require('querystring');
+const searchAudioTracks = async (track) => {
+    try {
+        let trackResponse;
+        trackResponse = await find(TRACKS_COLLECTION, { "name": new RegExp(track, 'i') }, TRACK_SEARCH_PROJECTION, {}, DEFAULT_LIMIT)
+
+        if (_.isEmpty(trackResponse)) {
+            const token = await getSpotifyToken()
+            trackResponse = await axios.get(SPOTIFY_SEARCH_URL, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+                params: {
+                    q: track,
+                    type: 'track',
+                    limit: 10, // Number of results to return
+                },
+                paramsSerializer: params => querystring.stringify(params),
+            });
+            await upsertDocuments(TRACKS_COLLECTION, trackResponse.data.tracks.items, 'id')
+            trackResponse = trackResponse.data.tracks.items
+        }
+        return prepareResponse(200, trackResponse);
+
+    } catch (error) {
+        return prepareResponse(500, { error: 'Error fetching movie data', details: error.message });
+    }
+}
+module.exports = {
+    searchAudioTracks
+}
--- .netlify/services/apiService.js
+++ .netlify/services/apiService.js
@@ -1,8 +1,11 @@
 const NodeCache = require('node-cache');
-const { TMDB_API_ID, DEFAULT_LIMIT, API_COLLECTION } = require('../utils/constants');
+const { TMDB_API_ID, DEFAULT_LIMIT, API_COLLECTION, SPOTIFY_TOKEN_URL } = require('../utils/constants');
 const { find } = require('./db');
 const apiKeyCache = new NodeCache({ stdTTL: 24 * 60 * 60 }); // Cache for 1 day
+const axios = require('axios');
+const querystring = require('querystring');
 
+const spotifyCache = new NodeCache({ stdTTL: 3600 }); // Cache TTL in seconds
 async function getTMDBApiKey() {
     const cacheKey = 'TMDB_API_KEY';
 
@@ -25,6 +28,38 @@ async function getTMDBApiKey() {
 
     return apiKey[0].api_key;
 }
+
+const getSpotifyToken = async () => {
+    const cachedToken = spotifyCache.get('accessToken');
+    if (cachedToken) {
+        console.log("token fetched from cache");
+        return cachedToken;
+    }
+    console.log("token expired, fetching new token");
+
+    let cliendCreds = await find(API_COLLECTION, { "_id": "spotify_creds" }, {}, {}, DEFAULT_LIMIT);
+    if (!cliendCreds) {
+        throw new Error('Spotify credentials are not set');
+    }
+    try {
+        const response = await axios.post(SPOTIFY_TOKEN_URL, querystring.stringify({
+            grant_type: 'client_credentials',
+            client_id: cliendCreds[0].client_id,
+            client_secret: cliendCreds[0].client_secret,
+        }), {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+        const token = response.data.access_token;
+        spotifyCache.set('accessToken', token, response.data.expires_in); // Cache token with expiration time
+        return token;
+    } catch (error) {
+        console.error('Error fetching access token:', error);
+    }
+};
+
 module.exports = {
-    getTMDBApiKey
+    getTMDBApiKey,
+    getSpotifyToken
 }
