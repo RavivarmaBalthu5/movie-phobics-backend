10f1a973bd76639d7e5b5244d090be6aea4ba99e
Ravivarma <ryuk.noir@gmail.com>
2024-08-29 00:16:00 +0530

    refactoring

--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -1,9 +1,8 @@
+const { join } = require('path');
+
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-    // Download Firefox (default `skipDownload: true`).
-    firefox: {
-        skipDownload: false,
-    },
+    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
