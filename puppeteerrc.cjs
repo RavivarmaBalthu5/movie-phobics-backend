2896259f6927eacd7e2419d02a247ca70bf493ef
Ravivarma <ryuk.noir@gmail.com>
2024-08-29 00:25:13 +0530

    refactoring

--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -1,7 +1,7 @@
 const { join } = require('path');
 
 /**
- * @type {import("puppeteer").Configuration}
+ * @type {import("puppeteer-core").Configuration}
  */
 module.exports = {
     cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
