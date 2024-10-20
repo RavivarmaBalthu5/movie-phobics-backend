9ebddbecf9881122b6336dba4bba9a7324810bec
Ravivarma <ryuk.noir@gmail.com>
2024-10-20 20:17:44 +0530

    add cache for apis

--- .netlify/helpers/movies.js
+++ .netlify/helpers/movies.js
@@ -3,11 +3,19 @@ const { prepareResponse } = require("../utils/utils");
 const { isEmpty, uniqBy } = require("lodash");
 const { find, upsertDocuments } = require("../services/db");
 const axios = require('axios');
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
 const API_KEY = process.env.TMDB_API_KEY;
 
 const getTotalPagesCount = async () => {
     try {
+        const cacheKey = `totalPagesCount`
+        const cachedData = myCache.get(cacheKey);
+        if (cachedData) {
+            return cachedData;
+        }
         let response = await axios.get(`${BASE_URL}movie/now_playing?api_key=${API_KEY}&language=en-US`);
+        myCache.set(cacheKey, response?.data?.total_pages, 3600);
         return prepareResponse(200, (response?.data?.total_pages) / 5);
     } catch (error) {
         return prepareResponse(500, { error: 'Error fetching Total Pages', details: error.message });
@@ -16,6 +24,11 @@ const getTotalPagesCount = async () => {
 
 const getCurrentPageMovies = async (currentPage) => {
     try {
+        const cacheKey = currentPage
+        const cachedData = myCache.get(cacheKey);
+        if (cachedData) {
+            return cachedData;
+        }
         let searchResponse = [];
         const startingPage = (currentPage - 1) * 5 + 1;
 
@@ -28,6 +41,7 @@ const getCurrentPageMovies = async (currentPage) => {
         }
         const uniqueMovies = uniqBy(searchResponse, 'id');
         await upsertDocuments(MOVIE_COLLECTION, uniqueMovies, 'id');
+        myCache.set(cacheKey, uniqueMovies, 3600);
         return prepareResponse(200, uniqueMovies);
     } catch (error) {
         return prepareResponse(500, { error: 'Error fetching current page movies', details: error.message });
@@ -58,12 +72,18 @@ const searchMovie = async (movieName) => {
 
 const fetchTrailer = async (movieIdString) => {
     try {
+        const cacheKey = `movieIdString`
+        const cachedData = myCache.get(cacheKey);
+        if (cachedData) {
+            return cachedData;
+        }
         const movieId = parseInt(movieIdString, 10);
         // Search for the movie to get the ID
         const response = await axios.get(`${BASE_URL}movie/${movieId}/videos?api_key=${API_KEY}`);
         if (isEmpty(response?.data.results)) {
             return prepareResponse(400, { error: 'Movie videos not found' })
         }
+        myCache.set(cacheKey, response?.data.results, 300);
         return prepareResponse(200, response?.data.results);
 
     } catch (error) {
